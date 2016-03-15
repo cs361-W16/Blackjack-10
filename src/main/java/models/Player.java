e445e16a188de06d3880e4b29006afe7c22f64d1
anisimon <anisimon@oregonstate.edu>
2016-03-14 20:34:58 -0700

    Adding the split hand function

--- src/main/java/models/Player.java
+++ src/main/java/models/Player.java
@@ -25,4 +25,26 @@ public class Player extends Actor{
             Bet = Bet + Bet;
         }
     }
+
+    /**
+     * Created by Natasha
+     * Nawwaf is writing the hit and stay functions but I need them to use this.
+     */
+    public interface IBlackJackHand {
+        public int total();
+        public void stay();
+        int hit(Card card); // returns hand total
+        public IBlackJackHand split(); // produces two hands
+    }
+
+    @Override
+    public IBlackJackHand split() {
+        // Check if hand can be split. Need to make sure that the two cards same value
+        total = total / 2;
+
+        IBlackJackHand splitHand = new IBlackJackHand(new Card(total));
+        splitHand.hit(); // Add a card to the splitHand
+        hit(); // add new card to the current hand
+        return splitHand;
+    }
 }
