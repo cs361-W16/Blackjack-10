e06ca1a2efa3de2a4145f20e1c6a9a5b168c6e24
Cody <cody.parker.holliday@gmail.com>
2016-03-14 21:21:53 -0700

    Update testDealer.java
    Changing the tests around

--- src/test/java/models/testDealer.java
+++ src/test/java/models/testDealer.java
@@ -4,7 +4,7 @@ import static org.junit.Assert.*;
 /**
  * Created by cody on 3/14/16.
  */
-public class testDealer {
+public class testDealerStop(){
     void testPlay(){
         Dealer d = new Dealer();
         Card c = new Card(1, Suit.Spades);
@@ -13,6 +13,17 @@ public class testDealer {
         while(d.play() == 1){
             d.addCard(c);
         }
+
+        d.addCard(c);
+        assertEquals(d.play(), 0);
+    }
+}
+public class testDealerPlay(){
+    void testPlay(){
+        Dealer d = new Dealer();
+        Card c = new Card(1, Suit.Spades);
+
+
 
         d.addCard(c);
         assertEquals(d.play(), 1);
